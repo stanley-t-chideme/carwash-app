783f7f71cd07dd23953c3e73709afe634fc17d22
Stanley Chideme <stanleychideme123@outlook.com>
2025-05-30 21:01:56 +0200

    gluestack init

--- mobile-app/app/(tabs)/index.tsx
+++ mobile-app/app/(tabs)/index.tsx
@@ -1,10 +1,26 @@
 import { Image } from 'expo-image';
-import { Platform, StyleSheet } from 'react-native';
+import { Platform, StyleSheet, View } from 'react-native';
 
 import { HelloWave } from '@/components/HelloWave';
 import ParallaxScrollView from '@/components/ParallaxScrollView';
 import { ThemedText } from '@/components/ThemedText';
 import { ThemedView } from '@/components/ThemedView';
+
+// import { Avatar, AvatarBadge, AvatarFallbackText, AvatarImage } from "@/components/ui/avatar";
+	
+function Example() {
+  return (
+    <Avatar size="md" >
+          <AvatarFallbackText>Jane Doe</AvatarFallbackText>
+        <AvatarImage
+          source={{
+            uri: 'https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80',
+          }}
+        />
+        <AvatarBadge />
+        </Avatar>
+  );
+}
 
 export default function HomeScreen() {
   return (
@@ -18,11 +34,13 @@ export default function HomeScreen() {
       }>
       <ThemedView style={styles.titleContainer}>
         <ThemedText type="title">Welcome!</ThemedText>
-        {/* <div className="w-10 h-10 bg-red-500" /> */}
+        
         <HelloWave />
       </ThemedView>
       <ThemedView style={styles.stepContainer}>
-        <ThemedText type="subtitle">Step 1: Try it</ThemedText>
+        <View className="w-10 h-10 bg-red-500">
+          <Example />
+          </View>
         <ThemedText>
           Edit <ThemedText type="defaultSemiBold">app/(tabs)/index.tsx</ThemedText> to see changes.
           Press{' '}
--- mobile-app/babel.config.js
+++ mobile-app/babel.config.js
@@ -1,10 +1,18 @@
 module.exports = function (api) {
   api.cache(true);
+  api.cache(true);
   return {
     presets: [
       ["babel-preset-expo", { jsxImportSource: "nativewind" }],
       "nativewind/babel",
     ],
-    plugins: ['react-native-reanimated/plugin'],
+    plugins: ['react-native-reanimated/plugin', ["module-resolver", {
+      root: ["./"],
+
+      alias: {
+        "@": "./",
+        "tailwind.config": "./tailwind.config.js"
+      }
+    }]],
   };
 };
